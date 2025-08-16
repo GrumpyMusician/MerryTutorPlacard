47a12048c6739aeee3d7205ef5cceead19a24f7b
GrumpyMusician <aliang@stu.naperville203.org>
2025-08-16 10:02:18 -0500

    Make clock bigger, clock update more immediately

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ fullscreen.addEventListener('click', () => {
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
 function updateClock() {
-  const now = new Date();
-  let hours = now.getHours();
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12 || 12; // convert 0 to 12
-  document.getElementById('clock').textContent = `${hours}:${minutes} ${ampm}`;
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12; // convert 0 to 12
+    document.getElementById('clock').textContent = `${hours}:${minutes} ${ampm}`;
 }
 
 // update immediately then every minute
 updateClock();
-setInterval(updateClock, 60000);
+setInterval(updateClock, 5000);
